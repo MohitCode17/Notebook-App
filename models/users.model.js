f3cafbb613366e0cf936ee77fd69733616deb986
MohitCode17 <mohit.codes17@gmail.com>
2024-05-30 21:39:39 +0530

    add user model

---
+++ models/users.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
