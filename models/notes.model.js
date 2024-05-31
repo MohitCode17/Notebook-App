a181b12368a8a8cee29a73d270692e7addf57ccb
MohitCode17 <mohit.codes17@gmail.com>
2024-05-31 21:57:28 +0530

    add notes model

---
+++ models/notes.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const notesSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.model("Note", notesSchema);
+
+export default Note;
